53f2bd9ba3452b749c40362db70741fa7d1a8051
Baptiste <baptiste.chachura@me.com>
2024-01-16 17:03:00 +0100

    Theatre

---
+++ Theatre.cpp
@@ -0,0 +1,33 @@
+#include"Theatre.h"
+#include<iostream>
+#include<string>
+
+using namespace std ;
+
+
+Theatre::Theatre   (string auteur, string titre, string editeur, string isbn, string public_vise, string etats, string siecle){
+    this->auteur = auteur;
+    this->titre = titre;
+    this->editeur = editeur;
+    this->isbn = isbn ;
+    this->public_vise = public_vise ;
+    this->etats = etats;
+    this->siecle = siecle ;
+
+}
+string  Theatre::getSiecle(){
+    return this->siecle;
+}                                                                                                         
+void    Theatre::setSiecle(string siecle) {
+    this->siecle =siecle;
+}                                                                                           
+void    Theatre::affiche () {
+        cout<< "auteur : "<< auteur <<endl;
+    cout<< "titre : "<< titre <<endl;
+    cout<< "editeur : "<< editeur <<endl;
+    cout<< "isbn : "<< isbn <<endl;
+    cout<< "public viisé : "<< public_vise <<endl;
+    cout<< "etats : "<< etats <<endl;
+    cout<<"siecle : "<< siecle<< endl;
+
+}                                                                                                         
